d4bc27bb1b8b41da3aaba08359377d1c616fd59e
dd399 <dd399@njit.edu>
2020-04-23 21:20:58 -0400

    first

---
+++ src/build/precache-manifest.fc3f11c32d83cc26981d12ce1152144f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2877f671b7cc861d8e1cff7aa6b5f2cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15e3aa32fd6ed543c736",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "845f66628956a29fa82e",
+    "url": "/static/js/2.067124be.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/static/js/2.067124be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e3aa32fd6ed543c736",
+    "url": "/static/js/main.91e71734.chunk.js"
+  },
+  {
+    "revision": "e988c064c70788dbc50e",
+    "url": "/static/js/runtime-main.a56884a5.js"
+  }
+]);
